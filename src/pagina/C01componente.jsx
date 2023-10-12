9c17f96b64e1b05c65df67f971ff10183abf6bb4
Meredith <meredith.13alanis@gmail.com>
2023-10-11 19:19:40 -0500

    v03 def. comp.

---
+++ src/pagina/C01componente.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const C01componente = () => {
+  return (
+    <div>
+      <h1>componente 1</h1>
+      primer componente defanida....
+    </div>
+  )
+}
+
+export default C01componente
