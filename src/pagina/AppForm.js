34b62a99b3895def358044adf462970f977cd080
Meredith <meredith.13alanis@gmail.com>
2023-10-24 11:41:41 -0500

    v05 app.js y appfrom

--- src/pagina/AppForm.js
+++ src/pagina/AppForm.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 const AppForm = () => {
   return (
-		<div style="width: 350px; background: greenyellow; padding: 10px;">
-    <div style="background: red; padding: 10px; text-align: center;">
-			        <h1>AppFormrmr</h1>
-			        <p>Nombre</p>
-			        <p>Edad</p>
-			        <p>Genero</p>
-              <button type="button" name="Guardar">Guardar</button>
+    <div style={{background:"orange" , padding:"10xp" , texalign:"center"}}>
+      <h1>AppForm.js</h1>
+      <p>Nombre</p>
+      <p>Edad</p>
+      <p>Genero</p>
+      <p>Guardar</p>
     </div>
-  </div>
   )
 }
 
 export default AppForm
+
+
