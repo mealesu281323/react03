3f9b01f59d3d23ec0e74c8602866eeed49010055
Meredith <meredith.13alanis@gmail.com>
2023-10-24 01:48:58 -0500

    v04 app.js y appfrom

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          edit <code>src/</code>
+          edit <code>src/App.js</code> 
         </p>
-        
+        <a 
+        className="App-link"
+        href="https://resactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          learn React
+        </a>
         
         </header> 
     </div>
